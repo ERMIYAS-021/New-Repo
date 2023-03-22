a41943858e170657949170a0147db3d1dd91ac74
ERMIYAS-021 <ermiyasbi007@gmail.com>
2023-03-22 13:47:52 +0300

    My bullshit Project

---
+++ WEB.js
@@ -0,0 +1,17 @@
+// var menuIcon = document.getElementById("menuIcon");
+// var sideNav = document.getElementById("sideNav");
+// var menu = document.getElementById("menu");
+// menuIcon.onclick = function() {
+//         if (sideNav.style.right == "-260px") {
+//             sideNav.style.right = "0";
+//         } else {
+//             sideNav.style.right = "-260px";
+//         }
+//     }
+//     // function operator() {
+//     //     if (sideNav.style.right == "-260px") {
+//     //         sideNav.style.right = "0";
+//     //     } else {
+//     //         sideNav.style.right = "-260px";
+//     //     }
+//     // }
